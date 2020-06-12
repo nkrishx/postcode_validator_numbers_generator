0b155f77c3bac68e5f47dde9150d7427e1c74d65
nkrishx <naveen.garagakrishnamurthy2@mail.dcu.ie>
2020-06-13 00:25:55 +0100

    requirments and sys exit chnage

--- UK_postcode.py
+++ UK_postcode.py
@@ -1,8 +1,9 @@
 """
 Description: Code for formatting and validating UK postcodes as per,
-              https://en.wikipedia.org/wiki/Postcodes_in_the_United_Kingdom
-              Postcode and Geolocation information retrieved from http://postcodes.io/
-              API endpoints, methods and documentation available at http://postcodes.io/
+              https://en.wikipedia.org/wiki/Postcodes_in_the_United_Kingdom,
+              Postcode and Geolocation information retrieved from http://postcodes.io/ ,
+              API endpoints, methods and documentation available at http://postcodes.io/ ,
+              Requires geopy version 1.11.0, for cordinates retrival.
 
 author    : Naveen Krishnamurthy
 Date      : 11-06-2020
@@ -48,11 +49,10 @@ def postcode_data(postcode, extra_arg=None):
         A status '200' indicates succesful request response cycle.
     """
     try:
-        url = requests.get('https://api.postcodes.io/postcodes/' + postcode) #api call to postoces.io
+        url = requests.get('https://api.postcodes.io/postcodes/' + postcode) #api call to postcodes.io
         data_postcode = json.loads(url.text)
         if data_postcode["status"] != 200:
-            print("\nPost code not found or the post code may not be in use!")
-            sys.exit(1)
+            return False
         else:
             return data_postcode
     except requests.exceptions.RequestException as e:
@@ -82,21 +82,25 @@ def show_details(postcode):
         This is to show the information retrieved from the API request,
         To API retuens cordinate (latitude and longitude) values,
         To get the address from the cordinates python geopy is made use of,
-        To get the longitude and latitude we will use the Nominatium which requires no API key.
+        To get the longitude and latitude we will use the Nominatium which requires no API key
+        Conditional checks for data returned from the api call and for the validity of the postcodes are made.
     """
     uk_postcode_str = "https://en.wikipedia.org/wiki/Postcodes_in_the_United_Kingdom"
     print("Supplied Postcode : \"{}\"" .format(postcode.upper()))
     if postcode_valid(postcode):
         data_postcode = postcode_data(postcode)
-        result = data_postcode["result"]
-        geolocator = Nominatim()
-        location = geolocator.reverse(str(result["latitude"]) + "," + str(result["longitude"]))
-        print("Valid Postcode: {}".format(str(postcode_valid(postcode))))
-        print("Outward Code: {}".format(outward_code(postcode)))
-        print("Inward Code: {}".format(inward_code(postcode)))
-        print("\nAddress: \n{}".format(location.address))
-        print("\nParish: {}\n".format(result["parish"]))
-        print("Longitude: {} \nLatitude: {}\n".format(str(result["longitude"]), str(result["latitude"])))
+        if data_postcode != False:
+            result = data_postcode["result"]
+            geolocator = Nominatim()
+            location = geolocator.reverse(str(result["latitude"]) + "," + str(result["longitude"]))
+            print("Valid Postcode: {}".format(str(postcode_valid(postcode))))
+            print("Outward Code: {}".format(outward_code(postcode)))
+            print("Inward Code: {}".format(inward_code(postcode)))
+            print("\nAddress: \n{}".format(location.address))
+            print("\nParish: {}\n".format(result["parish"]))
+            print("Longitude: {} \nLatitude: {}\n".format(str(result["longitude"]), str(result["latitude"])))
+        else:
+            print("\nPost code not found or the post code may not be in use!")
     else:
         print("{} is not a valid postcode".format(postcode))
         print("Please visit {}".format(uk_postcode_str))
